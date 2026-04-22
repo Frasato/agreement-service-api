d56b6dc6c5924f30065b2259dfa2eb19db9b3f01
Frasato <gabriel.frasato@hotmail.com>
2026-04-22 10:55:26 -0300

    first commit

---
+++ user-service/src/test/java/com/frasato/user_service/CreateUserUseCaseTest.java
@@ -0,0 +1,12 @@
+package com.frasato.user_service;
+
+import org.junit.jupiter.api.Test;
+
+public class CreateUserUseCaseTest {
+
+    @Test
+    void shouldCreateUserSuccessfully(){
+        
+    }
+
+}
---
+++ user-service/src/test/java/com/frasato/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frasato.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
