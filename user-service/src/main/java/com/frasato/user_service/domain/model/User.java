0bcd97e44e7b0ad8dc7d5dc712bfec58c711535f
Frasato <gabriel.frasato@hotmail.com>
2026-04-23 16:01:29 -0300

    feat(user): add role field to user model

--- user-service/src/main/java/com/frasato/user_service/domain/model/User.java
+++ user-service/src/main/java/com/frasato/user_service/domain/model/User.java
@@ -7,16 +7,18 @@ public class User {
     private String document;
     private String address;
     private String password;
+    private String role;
 
     public User(){}
 
-    public User(String id, String name, String phone, String document, String address, String password) {
+    public User(String id, String name, String phone, String document, String address, String password, String role) {
         this.id = id;
         this.name = name;
         this.phone = phone;
         this.document = document;
         this.address = address;
         this.password = password;
+        this.role = role;
     }
 
     public String getId() {
@@ -65,6 +67,14 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 
     public void validateDocument(){
