ab96cc4625a01a4ca398742f8d73837d350f2814
Frasato <gabriel.frasato@hotmail.com>
2026-04-23 16:05:39 -0300

    feat(auth): integrate token service into login use case

--- user-service/src/main/java/com/frasato/user_service/application/usecases/LoginUserUseCase.java
+++ user-service/src/main/java/com/frasato/user_service/application/usecases/LoginUserUseCase.java
@@ -1,22 +1,29 @@
 package com.frasato.user_service.application.usecases;
 
+import com.frasato.user_service.application.dto.LoginResult;
 import com.frasato.user_service.domain.model.User;
 import com.frasato.user_service.domain.repository.UserRepository;
+import com.frasato.user_service.domain.service.TokenService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class LoginUserUseCase {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
 
-    public LoginUserUseCase(UserRepository userRepository, PasswordEncoder passwordEncoder){
+    public LoginUserUseCase(UserRepository userRepository, PasswordEncoder passwordEncoder, TokenService tokenService){
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.tokenService = tokenService;
     }
 
-    public User login(String document, String password){
+    public LoginResult login(String document, String password){
         if(document.isBlank()) throw new RuntimeException("");
-        return userRepository.findUserByDocument(document)
-                .filter(user -> passwordEncoder.matches(password, user.getPassword()))
+        User user = userRepository.findUserByDocument(document)
+                .filter(foundedUser -> passwordEncoder.matches(password, foundedUser.getPassword()))
                 .orElseThrow(() -> new RuntimeException("Document or Password was wrong"));
+
+        String token = tokenService.generateToken(user);
+        return new LoginResult(token, user.getId());
     }
 }
