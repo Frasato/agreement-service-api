a61b098bf0e76c2811824c9c0fe47c2f29f6427a
Frasato <gabriel.frasato@hotmail.com>
2026-04-23 16:04:34 -0300

    feat(auth): implement token service using JWT

---
+++ user-service/src/main/java/com/frasato/user_service/infra/security/JwtTokenService.java
@@ -0,0 +1,35 @@
+package com.frasato.user_service.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.frasato.user_service.domain.model.User;
+import com.frasato.user_service.domain.service.TokenService;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+public class JwtTokenService implements TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    @Override
+    public String generateToken(User user) {
+        return JWT.create()
+                .withIssuer("user-service")
+                .withSubject(user.getDocument())
+                .withClaim("role", user.getRole())
+                .withExpiresAt(Instant.now().plus(2, ChronoUnit.HOURS))
+                .sign(Algorithm.HMAC256(secret));
+    }
+
+    @Override
+    public String validateToken(String token) {
+        return JWT.require(Algorithm.HMAC256(secret))
+                .withIssuer("user-service")
+                .build()
+                .verify(token)
+                .getSubject();
+    }
+}
