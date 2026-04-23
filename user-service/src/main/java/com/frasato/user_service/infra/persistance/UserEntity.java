fc0d3b846edeef0a1c3709f93bd7f02411af7980
Frasato <gabriel.frasato@hotmail.com>
2026-04-23 12:09:19 -0300

    feat(user): add user entity for JPA persistence

---
+++ user-service/src/main/java/com/frasato/user_service/infra/persistance/UserEntity.java
@@ -0,0 +1,76 @@
+package com.frasato.user_service.infra.persistance;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String phone;
+    private String document;
+    private String address;
+    private String password;
+
+    public UserEntity() {}
+
+    public UserEntity(String id, String name, String phone, String document, String address, String password) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.document = document;
+        this.address = address;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
