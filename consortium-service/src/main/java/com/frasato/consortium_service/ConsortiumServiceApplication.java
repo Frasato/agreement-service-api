f5544314c8492ccbf969bceeccd64bba66e926ea
Frasato <gabriel.frasato@hotmail.com>
2026-04-23 16:15:01 -0300

    feat(service): add consortium service

---
+++ consortium-service/src/main/java/com/frasato/consortium_service/ConsortiumServiceApplication.java
@@ -0,0 +1,13 @@
+package com.frasato.consortium_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsortiumServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsortiumServiceApplication.class, args);
+	}
+
+}
---
+++ consortium-service/src/test/java/com/frasato/consortium_service/ConsortiumServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frasato.consortium_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsortiumServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
