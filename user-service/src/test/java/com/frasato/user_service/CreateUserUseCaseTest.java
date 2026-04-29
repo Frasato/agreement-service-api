e704f7b73a740daf9752474dc8c29a25e2781dbc
Frasato <gabriel.frasato@hotmail.com>
2026-04-28 22:17:58 -0300

    test(auth): add success teste for register new user

--- user-service/src/test/java/com/frasato/user_service/CreateUserUseCaseTest.java
+++ user-service/src/test/java/com/frasato/user_service/CreateUserUseCaseTest.java
@@ -1,12 +1,44 @@
 package com.frasato.user_service;
 
+import com.frasato.user_service.application.usecases.RegisterUserUseCase;
+import com.frasato.user_service.domain.model.User;
+import com.frasato.user_service.domain.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
-public class CreateUserUseCaseTest {
+@ExtendWith(MockitoExtension.class)
+class CreateUserUseCaseTest {
+
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+    @InjectMocks
+    private RegisterUserUseCase registerUserUseCase;
 
     @Test
     void shouldCreateUserSuccessfully(){
-        
-    }
+        User user = new User(
+                "1",
+                "Gabriel Silva dos Santos",
+                "17991568457",
+                "96586565326",
+                "Rua Aurora Forte Neves",
+                "123",
+                "ROLE_USER"
+        );
 
+        when(userRepository.saveUser(any(User.class))).thenReturn(user);
+        when(passwordEncoder.encode(any(CharSequence.class))).thenReturn("123-encoded");
+
+        User result = registerUserUseCase.register(user);
+        assertEquals(user.getName(), result.getName());
+    }
 }
