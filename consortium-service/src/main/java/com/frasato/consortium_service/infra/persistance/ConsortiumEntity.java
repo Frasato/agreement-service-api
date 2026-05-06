17df361ede3ac2d7927913ca1c046f3768b5f005
Frasato <gabriel.frasato@hotmail.com>
2026-05-06 16:56:39 -0300

    feat(entity): add entity model consortium

---
+++ consortium-service/src/main/java/com/frasato/consortium_service/infra/persistance/ConsortiumEntity.java
@@ -0,0 +1,83 @@
+package com.frasato.consortium_service.infra.persistance;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "consortium")
+public class ConsortiumEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String description;
+    private Integer price;
+    private Boolean active;
+    private Instant includedAt;
+
+    public ConsortiumEntity() {}
+
+    public ConsortiumEntity(String id, String name, String description, Integer price, Boolean active, Instant includedAt) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.active = active;
+        this.includedAt = includedAt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public Instant getIncludedAt() {
+        return includedAt;
+    }
+
+    public void setIncludedAt(Instant includedAt) {
+        this.includedAt = includedAt;
+    }
+
+    @PrePersist
+    private void setDate(){
+        this.setIncludedAt(Instant.now());
+    }
+}
