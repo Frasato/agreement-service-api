4fa6eb10727100b6bc3a3512680b49a1b07cbbbf
Frasato <gabriel.frasato@hotmail.com>
2026-05-06 15:32:06 -0300

    feat(auth): remove database dependency add role on JWT service

--- user-service/src/main/java/com/frasato/user_service/infra/security/SecurityFilter.java
+++ user-service/src/main/java/com/frasato/user_service/infra/security/SecurityFilter.java
@@ -1,7 +1,5 @@
 package com.frasato.user_service.infra.security;
 
-import com.frasato.user_service.domain.model.User;
-import com.frasato.user_service.domain.repository.UserRepository;
 import com.frasato.user_service.domain.service.TokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -12,7 +10,6 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import java.io.IOException;
 import java.util.List;
 
@@ -20,11 +17,9 @@ import java.util.List;
 public class SecurityFilter extends OncePerRequestFilter {
 
     private final TokenService tokenService;
-    private final UserRepository userRepository;
 
-    public SecurityFilter(TokenService tokenService, UserRepository userRepository){
+    public SecurityFilter(TokenService tokenService){
         this.tokenService = tokenService;
-        this.userRepository = userRepository;
     }
 
     @Override
@@ -33,11 +28,10 @@ public class SecurityFilter extends OncePerRequestFilter {
 
         if(token != null){
             String document = tokenService.validateToken(token);
-            User user = userRepository.findUserByDocument(document)
-                    .orElseThrow(() -> new RuntimeException("User not found!"));
+            String role = tokenService.getRole(token);
 
-            var authorities = List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole()));
-            var auth = new UsernamePasswordAuthenticationToken(user, null, authorities);
+            var authorities = List.of(new SimpleGrantedAuthority("ROLE_" + role));
+            var auth = new UsernamePasswordAuthenticationToken(document, null, authorities);
             SecurityContextHolder.getContext().setAuthentication(auth);
         }
 
