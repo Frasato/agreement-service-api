21ae95c22035ce954667257cd15ae1401b77da71
Frasato <gabriel.frasato@hotmail.com>
2026-05-06 16:57:22 -0300

    feat(consortium): add consortium repository contract

---
+++ consortium-service/src/main/java/com/frasato/consortium_service/domain/repository/ConsortiumRepository.java
@@ -0,0 +1,12 @@
+package com.frasato.consortium_service.domain.repository;
+
+import com.frasato.consortium_service.domain.model.Consortium;
+import java.util.List;
+import java.util.Optional;
+
+public interface ConsortiumRepository {
+    Consortium saveConsortium(Consortium consortium);
+    List<Consortium> getAllConsortium();
+    Consortium findConsortiumById(String id);
+    Optional<Consortium> findConsortiumByName(String name);
+}
