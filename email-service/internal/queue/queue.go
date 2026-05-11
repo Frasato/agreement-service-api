d012625e82846d6314b6ed0209a8f3fb39d20470
Frasato <gabriel.frasato@hotmail.com>
2026-05-11 13:40:49 -0300

    feat(queue): configure RabbitMQ queue

---
+++ email-service/internal/queue/queue.go
@@ -0,0 +1,40 @@
+package queue
+
+import (
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+var Channel *amqp.Channel
+
+func ConnectRabbitMq() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+
+	if err != nil {
+		log.Fatal("Failed to connect to RabbitMQ")
+	}
+
+	ch, err := conn.Channel()
+
+	if err != nil {
+		log.Fatal("Failed to open channel")
+	}
+
+	_, err = ch.QueueDeclare(
+		"emails",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	if err != nil {
+		log.Fatal("Failed to declare queue")
+	}
+
+	Channel = ch
+
+	log.Println("RabbitMQ connected")
+}
