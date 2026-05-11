f8295cf74f3e6f6058edfb0c07d512fcfffb4496
Frasato <gabriel.frasato@hotmail.com>
2026-05-11 16:06:17 -0300

    feat(email): add env on rabbitmq auth

--- email-service/internal/queue/queue.go
+++ email-service/internal/queue/queue.go
@@ -2,6 +2,7 @@ package queue
 
 import (
 	"log"
+	"os"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
@@ -9,7 +10,7 @@ import (
 var Channel *amqp.Channel
 
 func ConnectRabbitMq() {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	conn, err := amqp.Dial("amqp://" + os.Getenv("RABBIT_USER") + ":" + os.Getenv("RABBIT_PASSWORD") + "@localhost:5672/")
 
 	if err != nil {
 		log.Fatal("Failed to connect to RabbitMQ")
