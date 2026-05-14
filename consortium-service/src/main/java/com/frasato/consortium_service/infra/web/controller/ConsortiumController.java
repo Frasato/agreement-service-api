3d896e3e239d413282e3f12423caa33b5047e9dd
Frasato <gabriel.frasato@hotmail.com>
2026-05-14 00:06:32 -0300

    feat (web): add assembler on endpoints

--- consortium-service/src/main/java/com/frasato/consortium_service/infra/web/controller/ConsortiumController.java
+++ consortium-service/src/main/java/com/frasato/consortium_service/infra/web/controller/ConsortiumController.java
@@ -4,13 +4,15 @@ import com.frasato.consortium_service.application.usecase.CreateConsortiumUseCas
 import com.frasato.consortium_service.application.usecase.FindOneConsortiumUseCase;
 import com.frasato.consortium_service.application.usecase.ListConsortiumsUseCase;
 import com.frasato.consortium_service.domain.model.Consortium;
+import com.frasato.consortium_service.infra.web.assembler.ConsortiumAssembler;
 import com.frasato.consortium_service.infra.web.dto.RequestCreateConsortiumDto;
 import com.frasato.consortium_service.infra.web.dto.ResponseAllConsortiumDto;
 import com.frasato.consortium_service.infra.web.dto.ResponseConsortiumDto;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -19,51 +21,33 @@ public class ConsortiumController {
     private final CreateConsortiumUseCase createConsortiumUseCase;
     private final FindOneConsortiumUseCase findOneConsortiumUseCase;
     private final ListConsortiumsUseCase listConsortiumsUseCase;
+    private final ConsortiumAssembler consortiumAssembler;
 
-    public ConsortiumController(CreateConsortiumUseCase createConsortiumUseCase, FindOneConsortiumUseCase findOneConsortiumUseCase, ListConsortiumsUseCase listConsortiumsUseCase){
+    public ConsortiumController(CreateConsortiumUseCase createConsortiumUseCase, FindOneConsortiumUseCase findOneConsortiumUseCase, ListConsortiumsUseCase listConsortiumsUseCase, ConsortiumAssembler consortiumAssembler){
         this.createConsortiumUseCase = createConsortiumUseCase;
         this.findOneConsortiumUseCase = findOneConsortiumUseCase;
         this.listConsortiumsUseCase = listConsortiumsUseCase;
+        this.consortiumAssembler = consortiumAssembler;
     }
 
     @GetMapping("/all")
-    public ResponseEntity<List<ResponseAllConsortiumDto>> allConsortiums(){
-
-        List<ResponseAllConsortiumDto> response =
-                listConsortiumsUseCase.listAllConsortiums()
-                        .stream()
-                        .map(ResponseAllConsortiumDto::fromEntity)
-                        .toList();
-
+    public ResponseEntity<CollectionModel<EntityModel<ResponseAllConsortiumDto>>> allConsortiums(){
+        List<Consortium> consortiums = listConsortiumsUseCase.listAllConsortiums();
+        CollectionModel<EntityModel<ResponseAllConsortiumDto>> response = consortiumAssembler.allConsortiumAssembler(consortiums);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ResponseConsortiumDto> oneConsortium(@PathVariable("id") String id){
+    public ResponseEntity<EntityModel<ResponseConsortiumDto>> oneConsortium(@PathVariable("id") String id){
         Consortium consortium = findOneConsortiumUseCase.findOne(id);
-        ResponseConsortiumDto dto = new ResponseConsortiumDto(
-                consortium.getId(),
-                consortium.getName(),
-                consortium.getDescription(),
-                consortium.getPrice(),
-                consortium.getActive()
-        );
-
-        return ResponseEntity.ok(dto);
+        EntityModel<ResponseConsortiumDto> response = consortiumAssembler.oneConsortiumAssembler(consortium);
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/create")
-    public ResponseEntity<ResponseConsortiumDto> createConsortium(@RequestBody RequestCreateConsortiumDto createConsortiumDto){
+    public ResponseEntity<EntityModel<ResponseConsortiumDto>> createConsortium(@RequestBody RequestCreateConsortiumDto createConsortiumDto){
         Consortium consortium = createConsortiumUseCase.createNewConsortium(createConsortiumDto.name(), createConsortiumDto.description(), createConsortiumDto.price());
-
-        ResponseConsortiumDto responseConsortiumDto = new ResponseConsortiumDto(
-                consortium.getId(),
-                consortium.getName(),
-                consortium.getDescription(),
-                consortium.getPrice(),
-                consortium.getActive()
-        );
-
+        EntityModel<ResponseConsortiumDto> responseConsortiumDto = consortiumAssembler.createConsortiumAssembler(consortium);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseConsortiumDto);
     }
 }
