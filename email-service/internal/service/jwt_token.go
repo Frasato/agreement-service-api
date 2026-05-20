aa8625bed7fdb1bf0beb536afb2dbd4280495c17
Frasato <gabriel.frasato@hotmail.com>
2026-05-19 22:58:47 -0300

    feat: add validate token function

---
+++ email-service/internal/service/jwt_token.go
@@ -0,0 +1,45 @@
+package service
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var secretKey = []byte("secret-key")
+
+func ValidateToken(tokenString string) (string, error) {
+	tokenString = RemovePrefix(tokenString)
+
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("Wrong Algorithm")
+		}
+
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+
+	if !ok || !token.Valid {
+		return "", errors.New("Invalid token")
+	}
+
+	if claims["iss"] != "user-service" {
+		return "", errors.New("Invalid issuer")
+	}
+
+	role, _ := claims["role"].(string)
+
+	return role, nil
+}
+
+func RemovePrefix(token string) string {
+	token = strings.TrimPrefix("Bearer ", token)
+	return token
+}
