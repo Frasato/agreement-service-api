47ff9cad102e80b28fc355864948828833cf2289
Frasato <gabriel.frasato@hotmail.com>
2026-05-20 16:23:38 -0300

    feat: add middleware on email route

--- email-service/internal/routes/routes.go
+++ email-service/internal/routes/routes.go
@@ -2,10 +2,11 @@ package routes
 
 import (
 	"email-service/internal/handlers"
+	"email-service/internal/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetUpRoutes(router *gin.Engine) {
-	router.POST("/email", handlers.SendEmail)
+	router.POST("/email", middleware.LoginMiddleware(), handlers.SendEmail)
 }
