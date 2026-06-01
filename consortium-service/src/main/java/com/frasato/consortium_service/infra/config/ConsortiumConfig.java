d3b24c4a6a1cc771f13d79da73c0f7345ea29294
Frasato <gabriel.frasato@hotmail.com>
2026-06-01 16:14:12 -0300

    feat: add bean to inject historic on create consortium

--- consortium-service/src/main/java/com/frasato/consortium_service/infra/config/ConsortiumConfig.java
+++ consortium-service/src/main/java/com/frasato/consortium_service/infra/config/ConsortiumConfig.java
@@ -2,17 +2,20 @@ package com.frasato.consortium_service.infra.config;
 
 import com.frasato.consortium_service.application.usecase.CreateConsortiumUseCase;
 import com.frasato.consortium_service.application.usecase.FindOneConsortiumUseCase;
+import com.frasato.consortium_service.application.usecase.HistoricProducerUseCase;
 import com.frasato.consortium_service.application.usecase.ListConsortiumsUseCase;
 import com.frasato.consortium_service.domain.repository.ConsortiumRepository;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import tools.jackson.databind.ObjectMapper;
 
 @Configuration
 public class ConsortiumConfig {
 
     @Bean
-    public CreateConsortiumUseCase createConsortiumUseCase(ConsortiumRepository repository) {
-        return new CreateConsortiumUseCase(repository);
+    public CreateConsortiumUseCase createConsortiumUseCase(ConsortiumRepository repository, HistoricProducerUseCase historicProducerUseCase) {
+        return new CreateConsortiumUseCase(repository, historicProducerUseCase);
     }
 
     @Bean
@@ -23,6 +26,11 @@ public class ConsortiumConfig {
     @Bean
     public ListConsortiumsUseCase listConsortiumsUseCase(ConsortiumRepository repository) {
         return new ListConsortiumsUseCase(repository);
+    }
+
+    @Bean
+    public HistoricProducerUseCase historicProducerUseCase(AmqpTemplate amqpTemplate, ObjectMapper objectMapper){
+        return new HistoricProducerUseCase(amqpTemplate, objectMapper);
     }
 
 }
