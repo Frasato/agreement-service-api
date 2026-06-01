153017b8d77ea2f1935241384f721d51dafaba89
Frasato <gabriel.frasato@hotmail.com>
2026-06-01 16:11:59 -0300

    feat: add user id on create consortium

--- consortium-service/src/main/java/com/frasato/consortium_service/infra/web/dto/RequestCreateConsortiumDto.java
+++ consortium-service/src/main/java/com/frasato/consortium_service/infra/web/dto/RequestCreateConsortiumDto.java
@@ -1,6 +1,7 @@
 package com.frasato.consortium_service.infra.web.dto;
 
 public record RequestCreateConsortiumDto(
+        String userId,
         String name,
         String description,
         Integer price
